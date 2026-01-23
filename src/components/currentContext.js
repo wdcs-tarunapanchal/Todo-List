ae8755710c0a6c9bca5020bb8e3a42677790dcd9
taruna.panchal <taruna.panchal@codezeros.com>
2026-01-23 15:31:02 +0530

    Test

--- src/components/currentContext.js
+++ src/components/currentContext.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const CurrentContext = React.createContext();
 
-const CurrentProvider = CurrentContext.Provider
-const CurrentConsumer = CurrentContext.Consumer
-
-export { CurrentConsumer, CurrentProvider , CurrentContext }
+const CurrentProvider = CurrentContext.Provider;
+const CurrentConsumer = CurrentContext.Consumer;
+// Alternatively, you can also export them like this:
+export { CurrentConsumer, CurrentContext, CurrentProvider };
