1b846e011c2e9b48f2f4f8be06b242619bc6f17c
Taruna Panchal <65295210+wdcs-tarunapanchal@users.noreply.github.com>
2026-01-29 16:55:27 +0530

    Revert "Release/1.0"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import { useState } from "react";
-import "./App.css";
-import Todo from "./components/Todo";
-import { CurrentProvider } from "./components/currentContext";
-import { EditProvider } from "./components/editContext";
-import { TaskProvider } from "./components/taskContext";
+import './App.css';
+import Todo from './components/Todo';
+import { TaskProvider } from './components/taskContext';
+import { useState } from 'react'
+import { EditProvider } from './components/editContext';
+import { CurrentProvider } from './components/currentContext';
 
 function App() {
-  // Feature 1: State to hold the list of tasks
   const [task, setTask] = useState([]);
-
-  // Feature 1: State to track if we are in editing mode
+  
   const [editing, setEditing] = useState(false);
 
-  // Feature 1: State to hold the current task being edited
-  const initialTodo = { id: null, task_name: "", status: "" };
+  const initialTodo = { id: null, task_name: '', status: '' };
 
   const [currentTask, setCurrentTask] = useState(initialTodo);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>Todo Task</p>
+        <p>
+          Todo Task
+        </p>
       </header>
       <TaskProvider value={[task, setTask]}>
         <EditProvider value={[editing, setEditing]}>
--- src/components/List.js
+++ src/components/List.js
@@ -1,73 +1,63 @@
-import { useContext } from "react";
-import Button from "react-bootstrap/Button";
-import Table from "react-bootstrap/Table";
-import { CurrentContext } from "./currentContext";
-import { EditContext } from "./editContext";
-import { TaskContext } from "./taskContext";
+import React, { useContext } from 'react'
+import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
+import { TaskContext } from './taskContext';
+import { CurrentContext } from './currentContext';
+import { EditContext } from './editContext';
 
 function List() {
-  // const List = (props) => {
-  // Feature 2: Use context to get tasks and state setters
-  const [task, setTask] = useContext(TaskContext);
-  const [currentTask, setCurrentTask] = useContext(CurrentContext);
-  const [editing, setEditing] = useContext(EditContext);
+    // const List = (props) => {
+    const [task, setTask] = useContext(TaskContext);
+    const [currentTask, setCurrentTask] = useContext(CurrentContext);
+    const [editing, setEditing] = useContext(EditContext);
 
-  // Feature 2: Function to delete a task
-  const deleteTodo = (inx) => {
-    var newArr = task.filter(function (ele) {
-      return ele.id != inx;
-    });
-    setTask(newArr);
-  };
+    const deleteTodo = inx => {
+        var newArr = task.filter(function (ele) {
+            return ele.id != inx;
+        });
+        setTask(newArr);
+    }
 
-  const editTodo = (id, task) => {
-    setEditing(true);
-    setCurrentTask(task);
-  };
+    const editTodo = (id, task) => {
+        setEditing(true);
+        setCurrentTask(task);
+    }
 
-  return (
-    <div>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Task</th>
-            <th>Status</th>
-            <th> Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {task.length > 0 ? (
-            task.map((todo) => {
-              const { id, task_name, status } = todo;
-              return (
-                <tr key={id}>
-                  <td>{task_name}</td>
-                  <td>{status}</td>
-                  <td>
-                    <Button
-                      variant="secondary"
-                      id={id}
-                      onClick={() => deleteTodo(id)}
-                    >
-                      Delete
-                    </Button>{" "}
-                    &nbsp;
-                    <Button variant="light" onClick={() => editTodo(id, todo)}>
-                      Edit
-                    </Button>
-                  </td>
-                </tr>
-              );
-            })
-          ) : (
-            <tr>
-              <td colSpan={4}>No task found</td>
-            </tr>
-          )}
-        </tbody>
-      </Table>
-    </div>
-  );
+    return (
+        <div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Task</th>
+                        <th>Status</th>
+                        <th> Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {task.length > 0 ? (
+                        task.map(todo => {
+                            const { id, task_name, status } = todo;
+                            return (
+                                <tr key={id}>
+                                    <td>{task_name}</td>
+                                    <td>{status}</td>
+                                    <td>
+                                        <Button variant="secondary" id={id} onClick={() => deleteTodo(id)}>Delete</Button> &nbsp;
+                                        <Button variant="light" onClick={() => editTodo(id, todo)}>Edit</Button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    ) : (
+                        <tr>
+                            <td colSpan={4}>No task found</td>
+                        </tr>
+                    )
+                    }
+                </tbody>
+            </Table>
+        </div>
+    )
 }
 
-export default List;
+export default List
